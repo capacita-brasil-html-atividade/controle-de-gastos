ba1babfc6547a062b2a6e2da98c52d6b35baae04
Joyce Silva <joycejsm96@protonmail.com>
2025-02-04 03:55:17 -0300

    add: adicionei nomes das categorias nos gráficos

--- Controle_de_gastos/src/componentes/grafico/Grafico.jsx
+++ Controle_de_gastos/src/componentes/grafico/Grafico.jsx
@@ -5,14 +5,14 @@ import { useGastos } from '../GastosContext';
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index, payload }) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
+    <text x={x} y={y} fill="black" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${payload.name}: ${(percent * 100).toFixed(0)}%`}
     </text>
   );
 };
