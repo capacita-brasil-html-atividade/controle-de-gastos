96e6b11e391d662ce6d2f46460a9a02d97be60d8
Joyce Silva <joycejsm96@protonmail.com>
2025-02-05 06:00:58 -0300

    adicionado estilo de componente grafico

--- Controle_de_gastos/src/componentes/grafico/Grafico.jsx
+++ Controle_de_gastos/src/componentes/grafico/Grafico.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 import { useGastos } from '../GastosContext';
-import "../Grafico/grafico.css"
+import styles from "../Grafico/grafico.module.css"
 
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#3F8042', '#00E56E'];
 
@@ -31,25 +31,31 @@ function Grafico() {
 
   return (
     <>
-    <h1>Grafico</h1>
-    <ResponsiveContainer width="100%" height={400}>
-      <PieChart>
-        <Pie
-          data={gastos}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-        >
-          {gastos.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
-    </ResponsiveContainer>
+    <span>
+      <h1>Grafico</h1>
+    </span>
+    <div className={styles.wrapperMaior}>
+      <div className={styles.wrapperMenor}>
+        <ResponsiveContainer width="100%" height={400}>
+          <PieChart>
+            <Pie
+              data={gastos}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {gastos.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
     </>
   );
   
